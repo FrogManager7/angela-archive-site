fcbf5844794b9d01e972af986ce212edfbc4aadb
FrogManager7 <192108540+FrogManager7@users.noreply.github.com>
2026-06-26 14:56:25 +0900

    add hamburger menu

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ noteButtons.forEach((button) => {
     }
   });
 });
+
+const menuButton = document.querySelector(".menu-button");
+const navMenu = document.querySelector(".nav-menu");
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+});
+
+document.addEventListener("click", (event) => {
+  //ボタンを押した時は何もしない
+  if (menuButton.contains(event.target)) return;
+  //メニューの中を押した時は何もしない
+  if (navMenu.contains(event.target)) return;
+  //それ以外なら閉じる
+  navMenu.classList.remove("open");
+})
+
+const navLinks = document.querySelectorAll(".nav-menu a");
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+  });
+});
